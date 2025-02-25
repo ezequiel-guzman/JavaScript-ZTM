36e8ab86db6766d1ac53f9f7b6b8fc194a1f1ba7
EZEQUIEL GUZMAN <57122751+ezequiel-guzman@users.noreply.github.com>
2025-02-24 20:38:46 -0700

    Create script.js
    practice

---
+++ script.js
@@ -0,0 +1,10 @@
+var age = prompt("what is your age?");
+if (Number(age) < 18) {
+    alert("too young");
+    } 
+    else if (Number(age) > 18) {
+            alert("enjoy");
+        }
+         else if (Number(age) === 18) {
+            alert("happy b day");
+            }
